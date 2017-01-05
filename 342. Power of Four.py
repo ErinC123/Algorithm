b8384c8ec4c5acb69d678be7dd066c1fe300b926
Zhen <zhencao93@gmail.com>
2017-01-05 14:31:51 -0500

    342. Power of Four

---
+++ 342. Power of Four.py
@@ -0,0 +1,26 @@
+#   Question: 342. Power of Four
+# Difficulty: Easy
+#       Tags: Bit Manipulation
+'''
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example:
+Given num = 16, return true. Given num = 5, return false.
+
+Follow up: Could you solve it without loops/recursion?
+'''
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return False
+        if num == 1:
+            return True
+        while num%4 == 0:
+            num = num/4
+            if num == 1:
+                return True
+        return False
